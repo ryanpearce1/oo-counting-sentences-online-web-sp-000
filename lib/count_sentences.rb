674098fa46e5964efb803d26cd43a4f6c64faa23
Ryan Pearce <ryan.pearce@key.me>
2021-01-15 15:13:17 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with? "."
+        true 
+      else
+        false 
   end
+end
 
   def question?
-
+    if self.end_with? "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with? "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if self.count > 3
+      true
+    else
+      false
   end
+end
+  
 end
